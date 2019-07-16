bcbbe3ab6ee9ac6f3e3c2525b6009772cfd6facc
votkca <230857512@qq.com>
2019-07-16 16:35:56 +0800

    second commit: simplify

--- jump7.py
+++ jump7.py
@@ -1,9 +1,7 @@
 for a in range(0,11):
     for b in range(0,10):
         c = 10 * a + b
-        if a == 7 or b == 7:
-            continue
-        elif c % 7 == 0:
+        if a == 7 or b == 7 or c % 7 ==0:
             continue
         elif c > 100 or c ==0:
             break
